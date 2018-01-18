31d06c0d7fc36844fdbd24d0a69f946c08116729
Safbek <safbek@list.ru>
2018-01-18 20:36:39 +0300

    1.Сверстала страницу task_list

--- application/views/board_module/task_list_view.php
+++ application/views/board_module/task_list_view.php
@@ -1,4 +1,4 @@
-	<?php #print_r($task_list); ?>
+<!-- <?php #print_r($task_list); ?>
 	
 	<div class="list_new_item">
 		<a href=/index.php/new_task>Добавить новую задачу</a>
@@ -27,4 +27,62 @@
 		</div>
 	</div>
 
-<?php endforeach;?>
+<?php endforeach;?> 
+-->
+
+<div class="main_form">
+				<section class="main">
+
+					<div class="form_list_item list_item">
+						<div class="cell ce"><?php echo '' ,$task['task_name']; ?></div>
+						<div class="cell ce"><?php echo 'Категория: ' ,$task['category_name']; ?></div>
+						<div class="cell ce block_green"><?php echo 'Статус: ' ,$task['status_name']; ?></div>
+						<div class="ce cell_icons">
+							<div class="edit"><?php echo '<a href=/task?task_id=', $task['task_id'] , '><img src="image/icon_edit.png" alt=""></a>'; ?></div>
+							<div class="delete"><?php echo '<a href=/cmd_delete_task?task_id=', $task['task_id'] , '><img src="image/icon_delete.png" alt=""></a>'; ?></div>
+						</div>
+					</div>
+
+					<div class="form_list_item list_item">
+						<div class="cell ce">Настройки работы DNS</div>
+						<div class="cell ce">Консул</div>
+						<div class="cell ce block_orange"></div>
+						<div class="ce cell_icons">
+							<div class="edit"><img src="image/icon_edit.png" alt=""></div>
+							<div class="delete"><img src="image/icon_delete.png" alt=""></div>
+						</div>
+					</div>
+
+
+					<div class="form_list_item list_item">
+						<div class="cell ce">Системы контроля за кодом и изменениями</div>
+						<div class="cell ce">Разработка Веб приложений</div>
+						<div class="cell ce block_grey"></div>
+						<div class="ce cell_icons">
+							<div class="edit"><img src="image/icon_edit.png" alt=""></div>
+							<div class="delete"><img src="image/icon_delete.png" alt=""></div>
+						</div>
+					</div>
+
+
+					<div class="form_list_item list_item">
+						<div class="ce cell">Тестовая работа</div>
+						<div class="ce cell">Тестирование</div>
+						<div class="ce cell block_red"></div>
+						<div class="ce cell_icons">
+							<div class="edit"><img src="image/icon_edit.png" alt=""></div>
+							<div class="delete"><img src="image/icon_delete.png" alt=""></div>
+						</div>
+					</div>
+				</section>
+
+				<aside class='sidebar'>
+					<form action="" method="post" class='container_search'>
+						<input type="search" name="" placeholder="поиск" class="form_search" />
+						<input type="submit" name="" value="" class="search_icon" />
+					</form>
+
+					<a href="#" class=""><button class='form_button' style='width:100%'>Добавить</button></a><br>
+				</aside>
+			</div>
+
